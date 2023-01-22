2f78f4bf2f3d7273228c128cc1860d059a92ab65
tyler <tyler.tongseng@gmail.com>
2023-01-22 12:03:15 +0800

    Fetch Required Data For LatestMovie.jsx

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import useFetch from "./hook/useFetch";
+import LatestMovie from "./LatestMovie";
 
 function App() {
+  const api = "35358a0b33df9cdbafd8ad11d7ad3049";
+
+  //Fetch Now Playing Movies, pass data to <LatestMovie/>
   const { data, loading, error, refetch } = useFetch(
-    "https://api.themoviedb.org/3/movie/now_playing?api_key=35358a0b33df9cdbafd8ad11d7ad3049&language=en-US&page=1"
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${api}&language=en-US&page=1`
   );
   if (loading) return <h1> LOADING... </h1>;
   if (error) console.log(error);
 
   return (
     <div>
-      <h2>Movie App</h2>
-
+      <h2>Featured Video</h2>
+      <LatestMovie nowPlaying={data} />
       <button onClick={refetch}>Refetch</button>
-      <h1>{data?.results.map((d) => d.title)}</h1>
     </div>
   );
 }
---
+++ starter-code/src/LatestMovie.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const LatestMovie = (props) => {
+  return (
+    <div>
+      <h1>Now Playing Movie</h1>
+
+      <h1>
+        {props.nowPlaying?.results.map((d) => {
+          return (
+            <section className="removelater" key={d.id}>
+              <div key={d.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300/${
+                    d.poster_path || d.backdrop_path
+                  }`}
+                  alt={d.title}
+                />
+                <h2>{d.title}</h2>
+                <h2>{d.popularity}</h2>
+                <h2>{d.vote_average}</h2>
+              </div>
+            </section>
+          );
+        })}
+      </h1>
+    </div>
+  );
+};
+
+export default LatestMovie;
