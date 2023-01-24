c23f36e18dd3adfefce16de6b93f25b6ab492736
tyler <tyler.tongseng@gmail.com>
2023-01-24 16:51:24 +0800

    Move Content from App.js to Homepage

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import MovieCover from "./components/MovieCover";
-import useFetch from "./hook/useFetch";
-import LatestMovie from "./components/LatestMovie";
-import Upcoming from "./components/Upcoming";
-import { MovieCarousel } from "./components/Carousel/Carousel";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import MovieInner from "./pages/MovieInner";
@@ -11,22 +6,8 @@ import ErrorPage from "./pages/ErrorPage";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  const api = "35358a0b33df9cdbafd8ad11d7ad3049";
-
-  //Fetch Now Playing Movies, pass data to <LatestMovie/>
-  const { data, loading, error, refetch } = useFetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${api}&language=en-US&page=1`
-  );
-  if (loading) return <h1> LOADING... </h1>;
-  if (error) console.log(error);
-
   return (
     <div className="home">
-      <h1 className="nav">header nav bar</h1>
-      <LatestMovie nowPlaying={data} />
-      <MovieCarousel nowPlaying={data} />
-      <Upcoming />
-      {/*/////////////////////////////////////////////////// */}
       <Router>
         <NavBar />
         <Routes>
--- starter-code/src/pages/Homepage.jsx
+++ starter-code/src/pages/Homepage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import useFetch from "../hook/useFetch";
+import LatestMovie from "../components/LatestMovie";
+import Upcoming from "../components/Upcoming";
+import { MovieCarousel } from "../components/Carousel/Carousel";
+
 const Homepage = () => {
-  return <div>This is the Home Page</div>;
+  const api = "35358a0b33df9cdbafd8ad11d7ad3049";
+
+  //Fetch Now Playing Movies, pass data to <LatestMovie/>
+  const { data, loading, error, refetch } = useFetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${api}&language=en-US&page=1`
+  );
+  if (loading) return <h1> LOADING... </h1>;
+  if (error) console.log(error);
+
+  return (
+    <div className="home">
+      <LatestMovie nowPlaying={data} />
+      <MovieCarousel nowPlaying={data} />
+      <Upcoming />
+    </div>
+  );
 };
 
 export default Homepage;
