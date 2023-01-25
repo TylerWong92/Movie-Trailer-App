6b505337fc5603308fe45e168d512b7e9ff41cc1
tyler <tyler.tongseng@gmail.com>
2023-01-25 12:12:32 +0800

    Innerpage css styling

--- starter-code/src/pages/MovieInner.jsx
+++ starter-code/src/pages/MovieInner.jsx
@@ -15,7 +15,6 @@ const MovieInner = () => {
 
   return (
     <div>
-      {data ? console.log(JSON.stringify(data)) : <h1>error</h1>}
       <button
         onClick={() => {
           navigate("/");
@@ -23,32 +22,47 @@ const MovieInner = () => {
       >
         back to home
       </button>
-      <h1>{data && data.budget}</h1>
-      <div>
-        {data &&
-          data.genres.map((d, i) => {
-            return <h1 key={i.id}>{d.name}</h1>;
-          })}
+      <div className="containertop">
+        <div className="col">
+          <img
+            className="movieCover"
+            src={`https://image.tmdb.org/t/p/w500/${data && data.poster_path}`}
+          />
+        </div>
+        <div className="col">
+          <div className="genres">
+            {data &&
+              data.genres.map((d, i) => {
+                return <h1 key={i.id}>{d.name}</h1>;
+              })}
+          </div>
+          <h1>{data && data.original_title}</h1>
+          <h1>{data && data.popularity}</h1>
+          <h1>{data && data.vote_average}</h1>
+          <h1>{data && data.status}</h1>
+          <h1>{data && data.release_date}</h1>
+          <p>{data && data.overview}</p>
+          <h1>{data && data.homepage}</h1>
+        </div>
       </div>
+      <div className="mainwrap">
+        <div className="col1"></div>
+
+        <div className="col2"></div>
+      </div>
+
+      {data ? console.log(JSON.stringify(data)) : <h1>error</h1>}
+
       <img
         src={`https://image.tmdb.org/t/p/w500/${data && data.backdrop_path}`}
       />
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${data && data.poster_path}`}
-      />
-      <h1>{data && data.homepage}</h1>
-      <p>{data && data.overview}</p>
-      <h1>{data && data.popularity}</h1>
+
       <div>
         {data &&
           data.production_companies.map((d, i) => {
             return <h1 key={i.id}>{d.name}</h1>;
           })}
       </div>
-      <h1>{data && data.release_date}</h1>
-      <h1>{data && data.runtime}</h1>
-      <h1>{data && data.status}</h1>
-      <h1>{data && data.vote_average}</h1>
     </div>
   );
 };
